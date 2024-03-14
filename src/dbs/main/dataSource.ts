e57576261921a695235fa336b2e222ec6c477dcf
Dongwon KIm <dongwon000103@gmail.com>
2024-03-14 13:38:56 +0900

    fix: db host

--- src/dbs/main/dataSource.ts
+++ src/dbs/main/dataSource.ts
@@ -6,8 +6,8 @@ const isProd: boolean = process.env.NODE_ENV === 'PROD';
 
 export const AppDataSource = new DataSource({
     type: 'mysql',
-    host: 'localhost', //DB HOST
-    port: 3312, // DB 포트 mysql default port는 3306
+    host: isProd ? process.env.MAIN_DB_HOST : 'localhost', //DB HOST
+    port: isProd ? Number(process.env.MAIN_DB_PORT) : 3312, // DB 포트 mysql default port는 3306
     username: isProd ? process.env.MAIN_DB_USERNAME : 'root', // DB 접속시 계정
     password: isProd ? process.env.MAIN_DB_PASSWORD : 'root', // DB 접속시 비밀번호
     database: isProd ? process.env.MAIN_DB_NAME : 'toto', // DB내 사용하는 DATABASE
