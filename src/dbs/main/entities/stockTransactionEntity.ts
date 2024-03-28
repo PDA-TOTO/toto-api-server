639ed9fdbc5ff8b53b222e40f10b47509864863b
Dongwon KIm <dongwon000103@gmail.com>
2024-03-28 10:46:23 +0900

    tmp: stock with balance

--- src/dbs/main/entities/stockTransactionEntity.ts
+++ src/dbs/main/entities/stockTransactionEntity.ts
@@ -1,6 +1,11 @@
 import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
-import User from './userEntity';
 import CODE from './codeEntity';
+import Account from './accountEntity';
+
+export enum TransactionType {
+    BUY = 'BUY',
+    CELL = 'CELL',
+}
 
 @Entity('STOCK_TRANSACTION')
 export class StockTransaction {
@@ -13,9 +18,12 @@ export class StockTransaction {
     @Column({ name: 'amount', type: 'int', nullable: false })
     amount: number;
 
-    @ManyToOne(() => User)
-    @JoinColumn({ name: 'userId' })
-    user: User;
+    @Column({ name: 'transaction_type', type: 'enum', enum: TransactionType })
+    transactionType: TransactionType;
+
+    @ManyToOne(() => Account)
+    @JoinColumn({ name: 'accountId' })
+    account: Account;
 
     @ManyToOne(() => CODE)
     @JoinColumn({ name: 'code' })
--- src/services/balance/IBalanceService.ts
+++ src/services/balance/IBalanceService.ts
@@ -3,6 +3,7 @@ import { IService } from '../IService';
 import Account from '../../dbs/main/entities/accountEntity';
 import { StockBalance } from '../../dbs/main/entities/stockBalanceEntity';
 import User from '../../dbs/main/entities/userEntity';
+import { IStockService } from '../stock/IStockService';
 
 export type AccountResponse = {
     id: number;
@@ -13,6 +14,10 @@ export type AccountResponse = {
 export interface IBalanceService extends IService {
     accountRepository: Repository<Account>;
     stockBalanceRepository: Repository<StockBalance>;
+    stockService: IStockService;
 
     createAccount(user: User): Promise<AccountResponse>;
+    buyStock(userId: number, code: string, price: number, amount: number): Promise<void>;
+    cellStock(userId: number, code: string, price: number, amount: number): Promise<void>;
+    findByUserId(userId: number): Promise<Account | null>;
 }
