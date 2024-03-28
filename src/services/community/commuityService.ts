0bcc69ca295a16268343182e1d68dd8ba254b30f
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2024-03-28 13:44:57 +0900

    feat : community 조회 추가

--- src/services/community/commuityService.ts
+++ src/services/community/commuityService.ts
@@ -11,12 +11,38 @@ const stockFindByCode = async (code: string) => {
   return await stockRepository.findOneBy({ krxCode: code });
 };
 
+export const setInitialCommunity = async (code: string) => {
+  const stockCode: CODE | null = await stockFindByCode(code);
+  try {
+    if (stockCode) {
+      const currentDate = new Date();
+      const oneWeekLater = currentDate.getTime() + 7 * 24 * 60 * 60 * 1000;
+      const oneWeekLaterDate = new Date(oneWeekLater);
+      const data = await communityRepository.save({
+        code: stockCode,
+        voteTitle: `${stockCode.name}, 오를까?`,
+        yesCount: 0,
+        noCount: 0,
+        startDate: currentDate,
+        endDate: oneWeekLaterDate,
+      });
+    }
+  } catch (error) {
+    console.log("error:", error);
+  }
+};
+
 export const getCommunityFindByKrxCode = async (code: string) => {
   const stockCode: CODE | null = await stockFindByCode(code);
   if (stockCode) {
-    return await communityRepository.find({
+    let communityData = await communityRepository.find({
       where: { code: stockCode },
     });
+    if (communityData.length === 0) {
+      await setInitialCommunity(code);
+      communityData = await getCommunityFindByKrxCode(code);
+    }
+    return communityData;
   } else {
     return [];
   }
