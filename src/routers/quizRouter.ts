096a4c5748edd4c87e078a2f5489d4a960b4d563
parkjineon <truewords0113@naver.com>
2024-03-30 12:57:24 +0900

    Feat: Implement Login Routing

--- src/routers/quizRouter.ts
+++ src/routers/quizRouter.ts
@@ -1,69 +1,86 @@
-import express, { Router, Request, Response, NextFunction } from 'express';
-import { IQuizService } from '../services/quiz/IQuizService';
-import { authenticate } from '../middlewares/authenticate/authenticate';
-import { getLevelTest, getStockTest, updateExperience } from '../services/quiz/QuizService';
+import express, { Router, Request, Response, NextFunction } from "express";
+import { IQuizService } from "../services/quiz/IQuizService";
+import { authenticate } from "../middlewares/authenticate/authenticate";
+import {
+  getLevelTest,
+  getStockTest,
+  updateExperience,
+} from "../services/quiz/QuizService";
 
 const router: Router = express.Router();
 
-router.get('/test',authenticate, async (req: Request, res: Response, next: NextFunction) => {
+router.get(
+  "/test",
+  authenticate,
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const level = (req.user!.experience < 100 ? 0
-            : (req.user!.experience < 400 ? 1
-            : (req.user!.experience< 800 ? 2
-            : 3)))
-        
-        if(req.user){
-            const quiz = await getStockTest(level, req.user);
-            
-    
-            res.status(200).json({
-                success: true,
-                message: '레벨테스트 문제 가져오기 완료',
-                result: quiz
-            })
-        }else{
-            res.status(200).json({
-                success: true,
-                message: '유저가 없습니다.',
-                result: null
-            })
-        }
+      const level =
+        req.user!.experience < 100
+          ? 0
+          : req.user!.experience < 400
+          ? 1
+          : req.user!.experience < 800
+          ? 2
+          : 3;
 
-    } catch (err) {
-        next(err);
-    }
-});
-
-
-router.get('/level',authenticate, async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const quiz = await getLevelTest()
+      if (req.user) {
+        const quiz = await getStockTest(level, req.user);
 
         res.status(200).json({
-            success: true,
-            message: '레벨테스트 문제 가져오기 완료',
-            result: quiz
-        })
-        
-    } catch (err) {
-        next(err);
-    }
-});
-
-router.post('/experience', authenticate, async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const experience = await updateExperience(req.user!.id,req.body?.experience)
-
+          success: true,
+          message: "레벨테스트 문제 가져오기 완료",
+          result: quiz,
+        });
+      } else {
         res.status(200).json({
-            success: true,
-            message: '경험치 추가 완료',
-            result: experience
-        })
-        
+          success: true,
+          message: "유저가 없습니다.",
+          result: null,
+        });
+      }
     } catch (err) {
-        next(err);
+      next(err);
     }
-});
+  }
+);
 
+router.get(
+  "/level",
+  authenticate,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const quiz = await getLevelTest();
+
+      res.status(200).json({
+        success: true,
+        message: "레벨테스트 문제 가져오기 완료",
+        result: quiz,
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.post(
+  "/experience",
+  authenticate,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const experience = await updateExperience(
+        req.user!.id,
+        req.body?.experience
+      );
+
+      res.status(200).json({
+        success: true,
+        message: "경험치 추가 완료",
+        result: experience,
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
 export default router;
--- src/services/quiz/QuizService.ts
+++ src/services/quiz/QuizService.ts
@@ -1,10 +1,9 @@
-import { AppDataSource } from '../../dbs/main/dataSource';
-import dotenv from 'dotenv';
-import Quiz from '../../dbs/main/entities/quizEntity';
-import Quizlog from '../../dbs/main/entities/quizlogEntity';
-import User from '../../dbs/main/entities/userEntity';
-import { VisibleUser } from '../user/userServiceReturnType';
-import { In, Not } from 'typeorm';
+import { AppDataSource } from "../../dbs/main/dataSource";
+import dotenv from "dotenv";
+import Quiz from "../../dbs/main/entities/quizEntity";
+import Quizlog from "../../dbs/main/entities/quizlogEntity";
+import User from "../../dbs/main/entities/userEntity";
+import { VisibleUser } from "../user/userServiceReturnType";
 
 dotenv.config();
 
@@ -14,94 +13,88 @@ const userRepository = AppDataSource.getRepository(User);
 
 // 배열을 섞는 함수
 function shuffleArray(array: any[]): any[] {
-    const shuffledArray = [...array];
-    for (let i = shuffledArray.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
-    }
-    return shuffledArray;
+  const shuffledArray = [...array];
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  }
+  return shuffledArray;
 }
 
-
 //Get Stock Test Quiz
-export const getStockTest = async (level: number, userId: VisibleUser): Promise<Quiz[]> => {
+export const getStockTest = async (
+  level: number,
+  userId: VisibleUser
+): Promise<Quiz[]> => {
+  // 해당 레벨의 모든 퀴즈를 가져옵니다.
+  const allQuizzes = await quizRepository.find({
+    where: { level },
+  });
 
-    // 해당 레벨의 모든 퀴즈를 가져옵니다.
-    const allQuizzes = await quizRepository.find({
-        where: { level }
-    });
+  // 퀴즈 목록을 섞습니다.
+  const shuffledQuizzes = shuffleArray(allQuizzes);
 
-    // 퀴즈 목록을 섞습니다.
-    const shuffledQuizzes = shuffleArray(allQuizzes);
+  // 필요한 개수만큼 퀴즈를 선택합니다.
+  const selectedQuizzes = shuffledQuizzes.slice(0, 5);
 
-    // 필요한 개수만큼 퀴즈를 선택합니다.
-    const selectedQuizzes = shuffledQuizzes.slice(0, 5);
-
-    return selectedQuizzes;
+  return selectedQuizzes;
 };
-
-
 
 //Get Level Test Quiz
 export const getLevelTest = async (): Promise<Quiz[]> => {
+  const response: Quiz[] = [];
 
-    const response:Quiz[] = []
+  const Q0 = await quizRepository.find({
+    where: {
+      level: 0,
+    },
+    take: 3,
+  });
 
-    const Q0 = await quizRepository.find({
-        where: {
-            level : 0
-        },
-        take: 3
-    }) 
+  const Q1 = await quizRepository.find({
+    where: {
+      level: 1,
+    },
+    take: 3,
+  });
 
+  const Q2 = await quizRepository.find({
+    where: {
+      level: 2,
+    },
+    take: 2,
+  });
 
-    const Q1 = await quizRepository.find({
-        where: {
-            level : 1
-        },
-        take: 3
-    }) 
+  const Q3 = await quizRepository.find({
+    where: {
+      level: 3,
+    },
+    take: 2,
+  });
 
-    const Q2 = await quizRepository.find({
-        where: {
-            level : 2
-        },
-        take: 2
-    }) 
+  const quiz = response.concat(Q0).concat(Q1).concat(Q2).concat(Q3);
 
-    const Q3 = await quizRepository.find({
-        where: {
-            level : 3
-        },
-        take: 2
-    }) 
-
-
-    const quiz = response.concat(Q0).concat(Q1).concat(Q2).concat(Q3);
-
-    return quiz;
+  return quiz;
 };
-
 
 //Get Level Test Quiz
-export const updateExperience= async (id: number, experience:number): Promise<number> => {
+export const updateExperience = async (
+  id: number,
+  experience: number
+): Promise<number> => {
+  const user = await userRepository.findOne({
+    where: {
+      id: id,
+    },
+  });
 
-    const user = await userRepository.findOne({
-        where:{
-            id: id
-        }
-    });
+  if (user) {
+    user.experience += experience;
 
-    if(user){
-        user.experience += experience;
+    await userRepository.save(user);
 
-        await userRepository.save(user);
-
-        return user.experience;
-    }else{
-        return 0;
-    }
-
+    return user.experience;
+  } else {
+    return 0;
+  }
 };
-
-
