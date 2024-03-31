33bf444bee09cb43760111e985fed002675d6acc
Dongwon KIm <dongwon000103@gmail.com>
2024-04-01 08:33:08 +0900

    feat: my stock

--- src/routers/stockRouter.ts
+++ src/routers/stockRouter.ts
@@ -1,19 +1,17 @@
-import express, { Router, Request, Response, NextFunction } from "express";
-import axios from "axios";
-import { toDate } from "../utils/date/toDate";
-import { IStockService } from "../services/stock/IStockService";
-import { StockService } from "../services/stock/StockServiceImpl";
-import { AppDataSource } from "../dbs/main/dataSource";
-import { authenticate } from "../middlewares/authenticate/authenticate";
-import { query } from "express-validator";
-import validateHandler from "../middlewares/validateHandler/validateHandler";
-import ApplicationError from "../utils/error/applicationError";
-import INFO from "../dbs/main/entities/infoEntity";
+import express, { Router, Request, Response, NextFunction } from 'express';
+import axios from 'axios';
+import { toDate } from '../utils/date/toDate';
+import { IStockService } from '../services/stock/IStockService';
+import { StockService } from '../services/stock/StockServiceImpl';
+import { AppDataSource } from '../dbs/main/dataSource';
+import { authenticate } from '../middlewares/authenticate/authenticate';
+import { query } from 'express-validator';
+import validateHandler from '../middlewares/validateHandler/validateHandler';
+import ApplicationError from '../utils/error/applicationError';
+import INFO from '../dbs/main/entities/infoEntity';
 
-console.log("---- start stock ----");
-const stockService: IStockService = new StockService(
-  AppDataSource.createQueryRunner()
-);
+console.log('---- start stock ----');
+const stockService: IStockService = new StockService(AppDataSource.createQueryRunner());
 const router: Router = express.Router();
 
 // router.get('/', async (req: Request, res: Response, next: NextFunction) => {
@@ -23,159 +21,135 @@ const router: Router = express.Router();
 // });
 
 // 코스피, 코스닥 가져오기
-router.get(
-  "/index/majors",
-  async (req: Request, res: Response, next: NextFunction) => {
+router.get('/index/majors', async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const url = "https://m.stock.naver.com/api/index/majors";
-      const response = await axios.get(url);
-      return res.send(response.data);
+        const url = 'https://m.stock.naver.com/api/index/majors';
+        const response = await axios.get(url);
+        return res.send(response.data);
     } catch (err) {
-      next(new ApplicationError(500, "API 요청 문제"));
+        next(new ApplicationError(500, 'API 요청 문제'));
     }
-  }
-);
+});
 
-router.get(
-  "/transactions",
-  authenticate,
-  async (req: Request, res: Response, next: NextFunction) => {
+router.get('/transactions', authenticate, async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const { size, page } = req.query;
+        const { size, page } = req.query;
 
-      const result = await stockService.findStockTransactionByUserId(
-        req.user!.id,
-        Number(size),
-        Number(page)
-      );
-      return res.status(200).json({
-        success: true,
-        message: "거래 내역 불러오기",
-        result: result,
-      });
+        const result = await stockService.findStockTransactionByUserId(req.user!.id, Number(size), Number(page));
+        return res.status(200).json({
+            success: true,
+            message: '거래 내역 불러오기',
+            result: result,
+        });
     } catch (err) {
-      next(err);
+        next(err);
     }
-  }
-);
+});
 
-router.get(
-  "/search/cap",
-  async (req: Request, res: Response, next: NextFunction) => {
+router.get('/search/cap', async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const { size, page } = req.query;
+        const { size, page } = req.query;
 
-      const result = await stockService.getStocksOrderByCap(
-        Number(page),
-        Number(size)
-      );
-      return res.status(200).json({
-        success: true,
-        message: "시가 총액 순으로 불러오기",
-        result: result,
-      });
+        const result = await stockService.getStocksOrderByCap(Number(page), Number(size));
+        return res.status(200).json({
+            success: true,
+            message: '시가 총액 순으로 불러오기',
+            result: result,
+        });
     } catch (err) {
-      next(err);
+        next(err);
     }
-  }
-);
+});
 
-const searchMiddlewares = [
-  query("name").notEmpty().withMessage("종목명은 무조건 있어야 합니다."),
-  validateHandler,
-];
+const searchMiddlewares = [query('name').notEmpty().withMessage('종목명은 무조건 있어야 합니다.'), validateHandler];
 // 주식 명칭으로 검색
-router.get(
-  "/search",
-  searchMiddlewares,
-  async (req: Request, res: Response, next: NextFunction) => {
+router.get('/search', searchMiddlewares, async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const { name } = req.query;
-      if (!name || typeof name !== "string")
-        throw new Error("Server Internal Error");
+        const { name } = req.query;
+        if (!name || typeof name !== 'string') throw new Error('Server Internal Error');
 
-      const result = await stockService.searchStock(name);
+        const result = await stockService.searchStock(name);
 
-      res.status(200).json({
-        success: true,
-        message: "주식 검색 성공",
-        result: result,
-      });
+        res.status(200).json({
+            success: true,
+            message: '주식 검색 성공',
+            result: result,
+        });
     } catch (err) {
-      next(err);
+        next(err);
     }
-  }
-);
+});
 
-router.get(
-  "/:code/finance",
-  async (req: Request, res: Response, next: NextFunction) => {
+router.get('/:code/finance', async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const { code } = req.params;
-      const result = await stockService.getFinanceByCode(code);
-      res.status(200).json({
-        success: true,
-        message: "재무재표 가져오기 성공",
-        result: result,
-      });
+        const { code } = req.params;
+        const result = await stockService.getFinanceByCode(code);
+        res.status(200).json({
+            success: true,
+            message: '재무재표 가져오기 성공',
+            result: result,
+        });
     } catch (err) {
-      next(err);
+        next(err);
     }
-  }
-);
+});
 
-router.get(
-  "/:code",
-  async (req: Request, res: Response, next: NextFunction) => {
+router.get('/:code', async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const { code } = req.params;
-      const { prev, bundle } = req.query;
-      const result = await stockService.getInfoWithChart(
-        code,
-        toDate(prev?.toString()),
-        bundle !== "MONTH" && bundle !== "YEAR" ? "DAY" : bundle
-      );
+        const { code } = req.params;
+        const { prev, bundle } = req.query;
+        const result = await stockService.getInfoWithChart(
+            code,
+            toDate(prev?.toString()),
+            bundle !== 'MONTH' && bundle !== 'YEAR' ? 'DAY' : bundle
+        );
 
-      res.status(200).json({
-        success: true,
-        message: "주식 차트 가져오기 성공",
-        result: result,
-      });
+        res.status(200).json({
+            success: true,
+            message: '주식 차트 가져오기 성공',
+            result: result,
+        });
     } catch (err) {
-      next(err);
+        next(err);
     }
-  }
-);
+});
 
-router.get(
-  "/:stockId/news",
-  async (req: Request, res: Response, next: NextFunction) => {
+router.get('/:stockId/news', async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const url = `https://m.stock.naver.com/api/news/stock/${req.params.stockId}?pageSize=20&page=1`;
-      const response = await axios.get(url);
-      return res.send(response.data);
+        const url = `https://m.stock.naver.com/api/news/stock/${req.params.stockId}?pageSize=20&page=1`;
+        const response = await axios.get(url);
+        return res.send(response.data);
     } catch (err) {
-      next(new ApplicationError(500, "API 요청 문제"));
+        next(new ApplicationError(500, 'API 요청 문제'));
     }
-  }
-);
+});
 
 const infoRepo = AppDataSource.getRepository(INFO);
-router.get(
-  "/:stockId/info",
-  async (req: Request, res: Response, next: NextFunction) => {
+router.get('/:stockId/info', async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const response = await infoRepo.find({
-        where: { krxCode: req.params.stockId },
-      });
-      res.status(200).json(response);
+        const response = await infoRepo.find({
+            where: { krxCode: req.params.stockId },
+        });
+        res.status(200).json(response);
 
-      //   const response = await stockService.getStockInfo(req.params.stockId);
-      //   return res.status(200).json(response);
+        //   const response = await stockService.getStockInfo(req.params.stockId);
+        //   return res.status(200).json(response);
     } catch (error) {
-      next(error);
+        next(error);
     }
-  }
-);
+});
+
+router.get('/:code/my', authenticate, async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const result = await stockService.getMyStockInfo(req.params.code, req.user!.id);
+        return res.status(200).json({
+            success: true,
+            message: '내 주식 현황',
+            result: result,
+        });
+    } catch (err) {
+        next(err);
+    }
+});
 
 export default router;
--- src/services/stock/IStockService.ts
+++ src/services/stock/IStockService.ts
@@ -1,108 +1,104 @@
-import { Repository } from "typeorm";
-import { IService } from "../IService";
-import CODE from "../../dbs/main/entities/codeEntity";
-import {
-  StockTransaction,
-  TransactionType,
-} from "../../dbs/main/entities/stockTransactionEntity";
-import { IUserService } from "../user/IUserService";
-import Price from "../../dbs/main/entities/priceEntity";
-import Finance from "../../dbs/main/entities/financeEntity";
-import { IPortfolioService } from "../portfolio/IPortfolioService";
-import INFO from "../../dbs/main/entities/infoEntity";
+import { Repository } from 'typeorm';
+import { IService } from '../IService';
+import CODE from '../../dbs/main/entities/codeEntity';
+import { StockTransaction, TransactionType } from '../../dbs/main/entities/stockTransactionEntity';
+import { IUserService } from '../user/IUserService';
+import Price from '../../dbs/main/entities/priceEntity';
+import Finance from '../../dbs/main/entities/financeEntity';
+import { IPortfolioService } from '../portfolio/IPortfolioService';
+import INFO from '../../dbs/main/entities/infoEntity';
 
 export type LogStock = {
-  krxCode: string;
-  price: number;
-  amount: number;
+    krxCode: string;
+    price: number;
+    amount: number;
 };
 export type CreateStockTransactionLogRequest = {
-  stock: LogStock[];
-  portId: number;
-  transactionType: TransactionType;
+    stock: LogStock[];
+    portId: number;
+    transactionType: TransactionType;
 };
 
 export type FinanceResponse = {
-  code: string;
-  yymm: string;
-  rev: number;
-  income: number;
-  netincome: number;
-  roeVal: number;
-  eps: number;
-  lbltRate: number;
-  bps: number;
-  incomeRate: number;
-  incomeGrownthRate: number;
-  netincomeGrownthRate: number;
-  per: number;
-  cap: number;
-  pbr: number;
-  dividend: number;
-  dividendRate: number;
-  quickRatio: number;
-  consensus: number;
-  beta: number;
-  revGrownthRate: number;
-  netincomeRate: number;
+    code: string;
+    yymm: string;
+    rev: number;
+    income: number;
+    netincome: number;
+    roeVal: number;
+    eps: number;
+    lbltRate: number;
+    bps: number;
+    incomeRate: number;
+    incomeGrownthRate: number;
+    netincomeGrownthRate: number;
+    per: number;
+    cap: number;
+    pbr: number;
+    dividend: number;
+    dividendRate: number;
+    quickRatio: number;
+    consensus: number;
+    beta: number;
+    revGrownthRate: number;
+    netincomeRate: number;
 };
 
 export type StockChartResponse = {
-  chartLength: number;
-  chart: any[];
-  code: string;
-  name: string;
-  bundleUnit: string;
+    chartLength: number;
+    chart: any[];
+    code: string;
+    name: string;
+    bundleUnit: string;
 };
 
 export type GetStockTransactionsResponse = {
-  total: number;
-  size: number;
-  page: number;
-  lastPage: number;
-  data: StockTransaction[];
+    total: number;
+    size: number;
+    page: number;
+    lastPage: number;
+    data: StockTransaction[];
 };
 
 export type GetStockWithCapResponse = {
-  total: number;
-  size: number;
-  page: number;
-  lastPage: number;
-  data: {
-    code: string;
-    name: string;
-    cap: number;
-    yymm: string;
-  }[];
+    total: number;
+    size: number;
+    page: number;
+    lastPage: number;
+    data: {
+        code: string;
+        name: string;
+        cap: number;
+        yymm: string;
+    }[];
+};
+
+export type MyStockResponse = {
+    num: number;
+    avg: number;
 };
 
 export interface IStockService extends IService {
-  stockRepository: Repository<CODE>;
-  stockTransactionRepository: Repository<StockTransaction>;
-  financeRepository: Repository<Finance>;
-  priceRepository: Repository<Price>;
-  infoRepository: Repository<INFO>;
-  userService: IUserService;
-  portfolioService: IPortfolioService;
+    stockRepository: Repository<CODE>;
+    stockTransactionRepository: Repository<StockTransaction>;
+    financeRepository: Repository<Finance>;
+    priceRepository: Repository<Price>;
+    infoRepository: Repository<INFO>;
+    userService: IUserService;
+    portfolioService: IPortfolioService;
 
-  findByCode(code: string, isRelationFinance?: boolean): Promise<CODE | null>;
-  createLog(request: CreateStockTransactionLogRequest): Promise<void>;
-  getFinanceByCode(code: string): Promise<FinanceResponse>;
-  getInfoWithChart(
-    code: string,
-    after: Date | null,
-    bundleUnit?: "DAY" | "MONTH" | "YEAR" | undefined
-  ): Promise<StockChartResponse>;
-  findStockTransactionByUserId(
-    userId: number,
-    size?: number,
-    page?: number
-  ): Promise<GetStockTransactionsResponse>;
-  getRecentPrice(code: string): Promise<number>;
-  searchStock(name: string): Promise<CODE[]>;
-  getStocksOrderByCap(
-    page?: number,
-    size?: number
-  ): Promise<GetStockWithCapResponse>;
-  getStockInfo(stockId: string): Promise<INFO[]>;
+    findByCode(code: string, isRelationFinance?: boolean): Promise<CODE | null>;
+    createLog(request: CreateStockTransactionLogRequest): Promise<void>;
+    getFinanceByCode(code: string): Promise<FinanceResponse>;
+    getInfoWithChart(
+        code: string,
+        after: Date | null,
+        bundleUnit?: 'DAY' | 'MONTH' | 'YEAR' | undefined
+    ): Promise<StockChartResponse>;
+    findStockTransactionByUserId(userId: number, size?: number, page?: number): Promise<GetStockTransactionsResponse>;
+    getRecentPrice(code: string): Promise<number>;
+    searchStock(name: string): Promise<CODE[]>;
+    getStocksOrderByCap(page?: number, size?: number): Promise<GetStockWithCapResponse>;
+    getStockInfo(stockId: string): Promise<INFO[]>;
+    getMyStockInfo(code: string, userId: number): Promise<MyStockResponse>;
 }
