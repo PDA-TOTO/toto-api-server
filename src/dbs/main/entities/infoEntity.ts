d012a4d2a8d9e1bc0b4f1bb3b9dfe9c645b6c3d6
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2024-03-31 16:14:36 +0900

    feat : info entity 추가

---
+++ src/dbs/main/entities/infoEntity.ts
@@ -0,0 +1,35 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  OneToMany,
+  OneToOne,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity("INFO")
+export default class INFO {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    name: "krxCode",
+    type: "varchar",
+    length: "30",
+    comment: "종목 코드",
+    nullable: false,
+  })
+  krxCode: string;
+
+  @Column({
+    name: "INFO",
+    type: "varchar",
+    length: "100",
+    comment: "종목 개요",
+    nullable: false,
+  })
+  INFO: string;
+}
