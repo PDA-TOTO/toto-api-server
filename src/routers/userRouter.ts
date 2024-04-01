050be58e4e812cdf173d031615f1bfbc583e25e3
parkjineon <truewords0113@naver.com>
2024-04-01 10:15:50 +0900

    Feat: Catch Login Error

--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -1,119 +1,149 @@
-import express, { Router, Request, Response, NextFunction } from 'express';
-import { body } from 'express-validator';
-import validateHandler from '../middlewares/validateHandler/validateHandler';
-import { createToken } from '../utils/user/auth';
-import { authenticate } from '../middlewares/authenticate/authenticate';
-import { IUserService } from '../services/user/IUserService';
-import { UserService } from '../services/user/UserServiceImpl';
-import { AppDataSource } from '../dbs/main/dataSource';
-import ApplicationError from '../utils/error/applicationError';
+import express, { Router, Request, Response, NextFunction } from "express";
+import { body } from "express-validator";
+import validateHandler from "../middlewares/validateHandler/validateHandler";
+import { createToken } from "../utils/user/auth";
+import { authenticate } from "../middlewares/authenticate/authenticate";
+import { IUserService } from "../services/user/IUserService";
+import { UserService } from "../services/user/UserServiceImpl";
+import { AppDataSource } from "../dbs/main/dataSource";
+import ApplicationError from "../utils/error/applicationError";
 const router: Router = express.Router();
 
-console.log('---- start user ----');
-const userService: IUserService = new UserService(AppDataSource.createQueryRunner());
+console.log("---- start user ----");
+const userService: IUserService = new UserService(
+  AppDataSource.createQueryRunner()
+);
 
 const signUpValidator = [
-    body('email')
-        .trim()
-        .isEmail()
-        .withMessage('올바른 이메일 형식이 아닙니다')
-        .custom(async (email) => {
-            if (await userService.isExistByEmail(email)) throw new Error('해당 이메일은 이미 사용 중 입니다.');
-        }),
-    body('password')
-        .isLength({ min: 6, max: 30 })
-        .withMessage('비밀번호의 길이는 6~30 입니다.')
-        .matches(/^(?=.*[a-zA-Z])(?=.*[0-9])/)
-        .withMessage('비밀번호는 문자+숫자 조합입니다.'),
-    validateHandler,
+  body("email")
+    .trim()
+    .isEmail()
+    .withMessage("올바른 이메일 형식이 아닙니다")
+    .custom(async (email) => {
+      if (await userService.isExistByEmail(email))
+        throw new Error("해당 이메일은 이미 사용 중 입니다.");
+    }),
+  body("password")
+    .isLength({ min: 6, max: 30 })
+    .withMessage("비밀번호의 길이는 6~30 입니다.")
+    .matches(/^(?=.*[a-zA-Z])(?=.*[0-9])/)
+    .withMessage("비밀번호는 문자+숫자 조합입니다."),
+  validateHandler,
 ];
 
-router.post('/sign-up', signUpValidator, async (req: Request, res: Response, next: NextFunction) => {
+router.post(
+  "/sign-up",
+  signUpValidator,
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const { email, password } = req.body;
-        const result = await userService.emailSignUp(email, password);
+      const { email, password } = req.body;
+      const result = await userService.emailSignUp(email, password);
 
-        return res.status(200).json({
-            success: true,
-            message: '회원가입 완료',
-            result: result,
-        });
+      return res.status(200).json({
+        success: true,
+        message: "회원가입 완료",
+        result: result,
+      });
     } catch (err) {
-        next(err);
+      next(err);
     }
-});
+  }
+);
 
-router.post('/log-in', async (req: Request, res: Response, next: NextFunction) => {
+router.post(
+  "/log-in",
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const { email, password } = req.body;
-        const user = await userService.logIn(email, password);
+      const { email, password } = req.body;
+      const user = await userService.logIn(email, password);
 
-        const tokenMaxAge = 60 * 60 * 24 * 3;
-        const token = createToken(user, tokenMaxAge);
+      const tokenMaxAge = 60 * 60 * 24 * 3;
+      const token = createToken(user, tokenMaxAge);
 
-        res.cookie('authToken', token, {
-            httpOnly: true,
-            maxAge: tokenMaxAge * 1000,
-        });
-
-        return res.status(200).json({
-            success: true,
-            message: '로그인 완료',
-            result: user,
-        });
-    } catch (err) {
-        next(err);
-    }
-});
-
-router.post('/log-out', async (req: Request, res: Response, next: NextFunction) => {
-    res.cookie('authToken', '', {
+      res.cookie("authToken", token, {
         httpOnly: true,
-        expires: new Date(Date.now()),
+        maxAge: tokenMaxAge * 1000,
+      });
+
+      return res.status(200).json({
+        success: true,
+        message: "로그인 완료",
+        result: user,
+      });
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(200).json({
+          success: false,
+          message: err.message,
+          result: null,
+        });
+      }
+    }
+  }
+);
+
+router.post(
+  "/log-out",
+  async (req: Request, res: Response, next: NextFunction) => {
+    res.cookie("authToken", "", {
+      httpOnly: true,
+      expires: new Date(Date.now()),
     });
 
     return res.status(200).json({
-        success: true,
-        message: '로그아웃 완료',
+      success: true,
+      message: "로그아웃 완료",
     });
-});
+  }
+);
 
 const updateTendencyMiddlewares = [
-    authenticate,
-    body('point').isNumeric().withMessage('포인트는 숫자이어야 합니다.'),
-    validateHandler,
+  authenticate,
+  body("point").isNumeric().withMessage("포인트는 숫자이어야 합니다."),
+  validateHandler,
 ];
-router.post('/tendency', updateTendencyMiddlewares, async (req: Request, res: Response, next: NextFunction) => {
+router.post(
+  "/tendency",
+  updateTendencyMiddlewares,
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const tendency = await userService.updateTendency(req.user!.email, req.body.point);
-        return res.status(200).json({
-            success: true,
-            message: tendency,
-        });
+      const tendency = await userService.updateTendency(
+        req.user!.email,
+        req.body.point
+      );
+      return res.status(200).json({
+        success: true,
+        message: tendency,
+      });
     } catch (err) {
-        next(err);
+      next(err);
     }
-});
+  }
+);
 
 // TODO: 디테일하기 가져오기
-router.get('/my-info', authenticate, async (req: Request, res: Response, next: NextFunction) => {
+router.get(
+  "/my-info",
+  authenticate,
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const user = await userService.findByEmail(req.user!.email, true);
+      const user = await userService.findByEmail(req.user!.email, true);
 
-        if (!user) {
-            throw new ApplicationError(401, '해당 유저 존재하지 않음');
-        }
+      if (!user) {
+        throw new ApplicationError(401, "해당 유저 존재하지 않음");
+      }
 
-        return res.status(200).json({
-            success: true,
-            message: '사용자 정보 가져오기 완료',
-            result: {
-                ...user,
-                password: "Can't see",
-            },
-        });
+      return res.status(200).json({
+        success: true,
+        message: "사용자 정보 가져오기 완료",
+        result: {
+          ...user,
+          password: "Can't see",
+        },
+      });
     } catch (err) {
-        next(err);
+      next(err);
     }
-});
+  }
+);
 export default router;
--- src/services/user/UserServiceImpl.ts
+++ src/services/user/UserServiceImpl.ts
@@ -1,139 +1,165 @@
-import { Repository, QueryRunner } from 'typeorm';
-import User from '../../dbs/main/entities/userEntity';
-import { AccountResponse, IBalanceService } from '../balance/IBalanceService';
-import { IUserService, VisibleUserResponse } from './IUserService';
-import dotenv from 'dotenv';
-import bcrypt from 'bcrypt';
-import { Transaction } from '../transaction';
-import ApplicationError from '../../utils/error/applicationError';
-import { IPortfolioService } from '../portfolio/IPortfolioService';
-import { createService } from '../serviceCreator';
-import { BalanceService } from '../balance/BalanceServiceImpl';
-import { PortfolioService } from '../portfolio/PortfolioServiceImpl';
+import { Repository, QueryRunner } from "typeorm";
+import User from "../../dbs/main/entities/userEntity";
+import { AccountResponse, IBalanceService } from "../balance/IBalanceService";
+import { IUserService, VisibleUserResponse } from "./IUserService";
+import dotenv from "dotenv";
+import bcrypt from "bcrypt";
+import { Transaction } from "../transaction";
+import ApplicationError from "../../utils/error/applicationError";
+import { IPortfolioService } from "../portfolio/IPortfolioService";
+import { createService } from "../serviceCreator";
+import { BalanceService } from "../balance/BalanceServiceImpl";
+import { PortfolioService } from "../portfolio/PortfolioServiceImpl";
 
 dotenv.config();
 
 export class UserService implements IUserService {
-    userRepository: Repository<User>;
-    balanceService: IBalanceService;
-    portfolioService: IPortfolioService;
-    queryRunner: QueryRunner;
-    name: string = 'UserService';
+  userRepository: Repository<User>;
+  balanceService: IBalanceService;
+  portfolioService: IPortfolioService;
+  queryRunner: QueryRunner;
+  name: string = "UserService";
 
-    constructor(queryRunner: QueryRunner) {
-        this.setQueryRunner(queryRunner);
+  constructor(queryRunner: QueryRunner) {
+    this.setQueryRunner(queryRunner);
+  }
+
+  setQueryRunner(queryRunner: QueryRunner): void {
+    this.queryRunner = queryRunner;
+    this.userRepository = queryRunner.manager.getRepository(User);
+
+    this.balanceService = createService(
+      queryRunner,
+      BalanceService.name,
+      this,
+      this.name
+    ) as IBalanceService;
+    this.portfolioService = createService(
+      queryRunner,
+      PortfolioService.name,
+      this,
+      this.name
+    ) as IPortfolioService;
+  }
+
+  @Transaction()
+  async findById(id: number): Promise<User | null> {
+    return await this.userRepository.findOne({
+      where: { id: id },
+      relations: {
+        account: true,
+      },
+    });
+  }
+
+  @Transaction()
+  async isExistByEmail(email: string): Promise<boolean> {
+    return await this.userRepository.existsBy({ email: email });
+  }
+
+  @Transaction()
+  async emailSignUp(
+    email: string,
+    password: string
+  ): Promise<VisibleUserResponse> {
+    if (await this.isExistByEmail(email)) {
+      throw new ApplicationError(400, "이미 사용 중인 이메일");
     }
 
-    setQueryRunner(queryRunner: QueryRunner): void {
-        this.queryRunner = queryRunner;
-        this.userRepository = queryRunner.manager.getRepository(User);
+    // hashing
+    const saltRound: number = Number(process.env.SALT_ROUND);
+    const salt = await bcrypt.genSalt(saltRound);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-        this.balanceService = createService(queryRunner, BalanceService.name, this, this.name) as IBalanceService;
-        this.portfolioService = createService(queryRunner, PortfolioService.name, this, this.name) as IPortfolioService;
+    const user: User = await this.userRepository.save({
+      email: email,
+      password: hashedPassword,
+    });
+
+    const account: AccountResponse = await this.balanceService.createAccount(
+      user
+    );
+    await this.portfolioService.createPortfolio(
+      user.id,
+      "기본 포트폴리오",
+      [],
+      true
+    );
+
+    return this.mapToVisibleUser(user, account);
+  }
+
+  @Transaction()
+  async logIn(email: string, password: string): Promise<VisibleUserResponse> {
+    const user = await this.findByEmail(email);
+
+    if (!user) {
+      throw new ApplicationError(400, "이메일이 존재하지 않습니다.");
     }
 
-    @Transaction()
-    async findById(id: number): Promise<User | null> {
-        return await this.userRepository.findOne({
-            where: { id: id },
-            relations: {
-                account: true,
-            },
-        });
+    try {
+      const auth = await bcrypt.compare(password, user.password);
+      if (auth) {
+        return this.mapToVisibleUser(user);
+      } else {
+        throw new ApplicationError(400, "비밀번호가 일치하지 않습니다.");
+      }
+    } catch (err) {
+      throw new ApplicationError(400, "비밀번호가 일치하지 않습니다.");
     }
+  }
 
-    @Transaction()
-    async isExistByEmail(email: string): Promise<boolean> {
-        return await this.userRepository.existsBy({ email: email });
+  @Transaction()
+  async updateTendency(email: string, point: number): Promise<string> {
+    if (point <= 20) {
+      await this.userRepository.update({ email: email }, { tendency: 1 });
+      return "안정형";
+    } else if (point <= 40) {
+      await this.userRepository.update({ email: email }, { tendency: 2 });
+      return "안정추구형";
+    } else if (point <= 60) {
+      await this.userRepository.update({ email: email }, { tendency: 3 });
+      return "위험중립형";
+    } else if (point <= 80) {
+      await this.userRepository.update({ email: email }, { tendency: 4 });
+      return "적극투자형";
+    } else {
+      await this.userRepository.update({ email: email }, { tendency: 5 });
+      return "공격투자형";
     }
+  }
 
-    @Transaction()
-    async emailSignUp(email: string, password: string): Promise<VisibleUserResponse> {
-        if (await this.isExistByEmail(email)) {
-            throw new ApplicationError(400, '이미 사용 중인 이메일');
-        }
+  @Transaction()
+  async findByEmail(email: string, withPort?: boolean): Promise<User | null> {
+    return await this.userRepository.findOne({
+      where: { email: email },
+      relations: {
+        account: true,
+        portfolios: withPort,
+      },
+    });
+  }
 
-        // hashing
-        const saltRound: number = Number(process.env.SALT_ROUND);
-        const salt = await bcrypt.genSalt(saltRound);
-        const hashedPassword = await bcrypt.hash(password, salt);
-
-        const user: User = await this.userRepository.save({ email: email, password: hashedPassword });
-
-        const account: AccountResponse = await this.balanceService.createAccount(user);
-        await this.portfolioService.createPortfolio(user.id, '기본 포트폴리오', [], true);
-
-        return this.mapToVisibleUser(user, account);
-    }
-
-    @Transaction()
-    async logIn(email: string, password: string): Promise<VisibleUserResponse> {
-        const user = await this.findByEmail(email);
-
-        if (!user) {
-            throw new ApplicationError(400, '이메일이 존재하지 않습니다.');
-        }
-
-        try {
-            const auth = await bcrypt.compare(password, user.password);
-            if (auth) {
-                return this.mapToVisibleUser(user);
-            } else {
-                throw new ApplicationError(400, '비밀번호가 일치하지 않습니다.');
-            }
-        } catch (err) {
-            throw new ApplicationError(400, '비밀버호가 일치하지 않습니다.');
-        }
-    }
-
-    @Transaction()
-    async updateTendency(email: string, point: number): Promise<string> {
-        if (point <= 20) {
-            await this.userRepository.update({ email: email }, { tendency: 1 });
-            return '안정형';
-        } else if (point <= 40) {
-            await this.userRepository.update({ email: email }, { tendency: 2 });
-            return '안정추구형';
-        } else if (point <= 60) {
-            await this.userRepository.update({ email: email }, { tendency: 3 });
-            return '위험중립형';
-        } else if (point <= 80) {
-            await this.userRepository.update({ email: email }, { tendency: 4 });
-            return '적극투자형';
-        } else {
-            await this.userRepository.update({ email: email }, { tendency: 5 });
-            return '공격투자형';
-        }
-    }
-
-    @Transaction()
-    async findByEmail(email: string, withPort?: boolean): Promise<User | null> {
-        return await this.userRepository.findOne({
-            where: { email: email },
-            relations: {
-                account: true,
-                portfolios: withPort,
-            },
-        });
-    }
-
-    mapToVisibleUser = (user: User, account?: AccountResponse): VisibleUserResponse => {
-        let visibleUser: VisibleUserResponse = {
-            id: user.id,
-            account: user.account,
-            experience: user.experience,
-            email: user.email,
-            tendency: user.tendency,
-            createdAt: user.createdAt,
-        };
-
-        if (!account) {
-            return visibleUser;
-        }
-
-        return {
-            ...visibleUser,
-            account: account,
-        };
+  mapToVisibleUser = (
+    user: User,
+    account?: AccountResponse
+  ): VisibleUserResponse => {
+    let visibleUser: VisibleUserResponse = {
+      id: user.id,
+      account: user.account,
+      experience: user.experience,
+      email: user.email,
+      tendency: user.tendency,
+      createdAt: user.createdAt,
     };
+
+    if (!account) {
+      return visibleUser;
+    }
+
+    return {
+      ...visibleUser,
+      account: account,
+    };
+  };
 }
